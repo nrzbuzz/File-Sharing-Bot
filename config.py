5929343c7ddff2b44fccbeab6ef6e1febbaecee3
nrzbuzz <111190703+nrzbuzz@users.noreply.github.com>
2023-08-14 08:05:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID"1226291457""))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "https://t.me/pan_kukufm")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
